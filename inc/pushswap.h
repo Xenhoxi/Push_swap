8bc3d5c899dedee8bf226bca13a37e24bfa73a97
JERINECK Léon <ljerinec@lj.home>
2023-01-20 21:59:27 +0100

    ok

---
+++ inc/pushswap.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 12:03:02 by ljerinec          #+#    #+#             */
+/*   Updated: 2023/01/19 18:17:04 by ljerinec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAP_H
+
+# define PUSHSWAP_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct s_stack
+{
+	int				value;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}					t_stack;
+
+typedef struct s_stack_info
+{
+	struct s_stack	*first;
+	struct s_stack	*last;
+}					t_stack_info;
+
+#endif
