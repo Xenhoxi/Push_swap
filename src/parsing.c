c4abcbea9753a105328b1bbbc1d842082328e9cb
JERINECK Léon <ljerinec@lj.home>
2023-01-23 13:00:33 +0100

    damn

--- src/parsing.c
+++ src/parsing.c
@@ -6,35 +6,70 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 00:49:09 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/01/21 15:39:37 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/01/23 04:47:54 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pushswap.h"
 
-bool	check_argv()
+void	error(void)
 {
-	// Is_digit()
+	write(1, "Error\n", 6);
+	exit(1);
+}
+
+int	only_digit(char	**input_list, int argc)
+{
+	int	i;
+	int	u;
+
+	i = 1;
+	u = 0;
+	while (i <= argc)
+	{
+		while (input_list[i])
+		{
+			if ((input_list[i][u] >= '0' && input_list[i][u] <= '9')
+				|| input_list[i][u] == '-' || input_list[i][u] == ' ')
+				u++;
+			else
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	check_argv(char	**input_list, int argc)
+{
+	int	checker;
+
+	checker = 0;
+	if (only_digit(input_list, argc))
+		checker = 1;
 	// atoi
 	// Is_int()
 	// No_multiple_same_numbers()
+	return (checker);
 }
 
 int	parsing(int argc, char **argv)
 {
-	char	**numbers_list;
-	int		i;
+	char	**input_list;
 
-	i = 0;
+	if (argc <= 1)
+		error();
 	if (argc == 2)
 	{
-		numbers_list = ft_split(argv[1], ' ');
-		
-		// while (numbers_list[i])
-		// 	printf("%s\n", numbers_list[i++]);
-		//Check_argv()
-		return (0);
+		input_list = ft_split(argv[1], ' ');
+		if (check_argv(input_list, argc))
+			return (1);
+		else
+			error();
 	}
-	return (1);
+	if (argc > 2)
+	{
+		
+	}
+	return (0);
 }
- 
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:31:23 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/01/21 15:30:25 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/01/22 22:51:41 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,10 @@ t_stack	*fill_stack(int argc, char **argv)
 
 	i = 1;
 	stack = malloc(sizeof(t_stack));
-	stack->value = ft_atoi(argv[i++]);
+	stack->value = ft_atoi(argv[i]);
 	stack_first = stack;
-	while (i < argc)
-	{
-		while (stack->next)
-			stack = stack->next;
-		stack->next = malloc(sizeof(t_stack));
-		stack = stack->next;
-		stack->value = ft_atoi(argv[i]);
-		i++;
-	}
+	while (++i < argc)
+		push_back(stack, ft_atoi(argv[i]));
 	return (stack_first);
 }
 
@@ -55,16 +48,11 @@ void	print_stack(t_stack *stack)
 int	main(int argc, char **argv)
 {
 	t_stack			*stack1;
-	int				i;
 
-	i = 1;
-	stack1 = fill_stack(argc, argv);
-	push_back(stack1, 15);
-	print_stack(stack1);
+	if (parsing(argc, argv))
+	{
+		stack1 = fill_stack(argc, argv);
+		print_stack(stack1);
+	}
 	return (0);
 }
-
-	// if (parsing(argc, argv))
-	// 	printf("Error\n");
-	// else
-	// 	printf("Parsing OK !\n");
