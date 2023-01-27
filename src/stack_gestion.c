6af438aaf84adc9560a17208acce1c14013c538a
Leon Jerineck <ljerinec@k0r4p7.42mulhouse.fr>
2023-01-27 13:59:39 +0100

    jesuistropfort

--- src/stack_gestion.c
+++ src/stack_gestion.c
@@ -6,38 +6,31 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 02:59:53 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/01/27 12:35:13 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/01/27 13:56:56 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pushswap.h"
 
-t_stack	*push_back(t_stack *stack, int new_value)
+void	push_back(t_stack *stack, int new_value)
 {
 	t_stack	*tmp_prev;
-	t_stack	*stack_first;
 
-	tmp_prev = stack;
+	if (stack)
+		while (stack->next)
+			stack = stack->next;
 	stack->next = malloc(sizeof(t_stack));
+	tmp_prev = stack;
 	stack = stack->next;
-	stack_first = stack->first;
+	stack->next = 0;
 	stack->value = new_value;
 	stack->prev = tmp_prev;
-	stack->first = stack_first;
-	return (stack);
-}
-
-t_stack	*find_last(t_stack *stack)
-{
-	while (stack->next)
-		stack = stack->next;
-	return (stack);
 }
 
 t_stack	*fill_stack_split(int argc, char **argv)
 {
 	t_stack	*stack;
-	t_stack	*last;
+	t_stack	*stack_first;
 	int		i;
 
 	i = 0;
@@ -45,16 +38,17 @@ t_stack	*fill_stack_split(int argc, char **argv)
 	if (!stack)
 		return (NULL);
 	stack->value = ft_atoi(argv[i]);
-	stack->first = stack;
+	stack_first = stack;
+	stack->next = 0;
 	while (++i < argc)
-		stack = push_back(stack, ft_atoi(argv[i]));
-	last = find_last(stack);
-	return (stack->first);
+		push_back(stack, ft_atoi(argv[i]));
+	return (stack_first);
 }
 
 t_stack	*fill_stack_argv(int argc, char **argv)
 {
 	t_stack	*stack;
+	t_stack	*stack_first;
 	int		i;
 
 	i = 1;
@@ -62,10 +56,14 @@ t_stack	*fill_stack_argv(int argc, char **argv)
 	if (!stack)
 		return (NULL);
 	stack->value = ft_atoi(argv[i]);
-	stack->first = stack;
-	while (i < argc)
-		push_back(stack, ft_atoi(argv[++i]));
-	return (stack->first);
+	stack_first = stack;
+	stack->next = 0;
+	while (i < argc - 1)
+	{
+		i++;
+		push_back(stack, ft_atoi(argv[i]));
+	}
+	return (stack_first);
 }
 
 void	print_stack(t_stack *stack)
